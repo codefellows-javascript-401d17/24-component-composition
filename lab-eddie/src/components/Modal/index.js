5a25eac64e63dd2d9cbe6d17c7be7b9312cce1d3
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-08-24 23:27:00 -0700

    Added delete category functionality

---
+++ lab-eddie/src/components/Modal/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>
+
+      </li>
+    )
+  }
+}
+
+export default Modal;

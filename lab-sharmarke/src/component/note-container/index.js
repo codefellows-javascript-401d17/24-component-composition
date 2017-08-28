72b0a94a29947a0b3064a89c3ebc8de2716928c4
sharmarke <smarky292@gmail.com>
2017-08-27 21:20:32 -0700

    did some shit but not finished

---
+++ lab-sharmarke/src/component/note-container/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import uuid from 'uuid/v1';
+
+import NoteList from '../note-list';
+import NoteCreateForm from '../note-create-form';
+
+let renderIf = (test, component) => test ? component : undefined;
+
+class NoteContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showErrors: true
+    }
+    this.noteCreate = this.noteCreate.bind(this);
+    this.noteRemove = this.noteRemove.bind(this);
+    this.noteUpdate = this.noteUpdate.bind(this);
+  }
+
+  noteCreate(note) {
+    note.id = uuid();
+    let {app} = this.props;
+    app.setState(prevState => ({
+      notes: prevState.notes.concat([note])
+    }));
+  }
+
+  noteRemove(note) {
+    let {app} = this.props;
+    app.setState(prevState => ({
+      notes: prevState.notes.filter((item) => {
+        return item.id !== note.id
+      })
+    }))
+  }
+
+  noteUpdate(note) {
+    let {app} = this.props;
+    app.setState(prevState => ({
+      notes: prevState.notes.map((item) => {
+        return item.id === note.id ? expense : item;
+      })
+    }));
+  }
+}
+render() {
+  let {app} =this.props;
+
+  return (
+    <section className='note-container'>
+      <div>
+        <
+  )
+}
